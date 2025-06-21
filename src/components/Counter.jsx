68604814cf9063bb83c2a6905ceeaf66ad10c1b3
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-06-22 08:01:21 +0900

    4.16 useState 유의할점!

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,11 @@ export default function Counter() {
   const [count,setCount] = useState(0);
 
   const handleCounter = () => {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   return (
