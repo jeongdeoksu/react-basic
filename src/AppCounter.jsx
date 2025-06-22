b73ce5b21c9fe1b06cbf83e94e5253da2959e937
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-06-22 10:39:25 +0900

    4.18 카운터 상태 끌어 올리기

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './components/Counter.jsx'
 import './App.css'
 
 export default function AppCounter() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prev => prev + 1)
+  }
   return (
-      <div>
-        <Counter />
-      </div>
-  );
+
+        <div className="container">
+          <div className="banner">
+            Total Count: {count} {count > 10 ? '🔥': '❄️'}
+          </div>
+          <Counter total={count} onClick={handleClick} />
+          <Counter total={count} onClick={handleClick} />
+        </div>
+  )
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,16 @@
 import React, {useState} from 'react';
 
-export default function Counter() {
+export default function Counter({total, onClick}) {
   const [count,setCount] = useState(0);
 
   const handleCounter = () => {
     setCount((prevCount) => prevCount + 1);
-    setCount((prevCount) => prevCount + 1);
-    setCount((prevCount) => prevCount + 1);
-    setCount((prevCount) => prevCount + 1);
-    setCount((prevCount) => prevCount + 1);
+    onClick();
   }
 
   return (
       <div className="counter">
-        <span className="number">{count}</span>
+        <p className="number">{count} <span className='total'>/{total}</span></p>
         <button onClick={handleCounter} className="button">Add +</button>
       </div>
   );
